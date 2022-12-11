4e691b83ce153b5a1529bc237c5a914d493ff5e9
Mateusz Kretowski <matkretkurs@gmail.com>
2022-12-11 16:01:32 +0100

    Add addToFavorites function

--- src/js/script.js
+++ src/js/script.js
@@ -4,18 +4,48 @@
     templateOf: {
       bookItem: '#template-book',
     },
+    booksList: '.books-list',
   };
 
   const templates = {
     bookItem: Handlebars.compile(document.querySelector(select.templateOf.bookItem).innerHTML),
   };
 
-  const app = {
-    initCollection: function () {
-      const thisApp = this;
+  const coll = {
+    getElements() {
+      const thisColl = this;
+      thisColl.favoriteBooks = [];
+      thisColl.allBooks = document.querySelector(select.booksList);
+      console.log(thisColl.allBooks);
+    },
+    initActions: function () {
+      const thisColl = this;
+      console.log(thisColl);
+      /* add addToFavourites as event listener for all books */
+      thisColl.allBooks.addEventListener('dblclick', function (event) {
+        event.preventDefault();
+        thisColl.addToFavorites(event);
+      });
+    },
+    addToFavorites: function (event) {
+      const thisColl = this;
+      const thisBook = event.target.offsetParent;
+      if (thisBook.classList.contains('book__image')) {
+        if (thisColl.favoriteBooks.includes(thisBook.getAttribute('data-id'))) {
+          thisBook.classList.remove('favorite');
+          thisColl.favoriteBooks.splice(thisColl.favoriteBooks.indexOf(thisBook.getAttribute('data-id')), 1);
+        } else {
+          thisBook.classList.add('favorite');
+          thisColl.favoriteBooks.push(thisBook.getAttribute('data-id'));
+        }
+        console.log(thisColl.favoriteBooks);
+      }
+    },
+    renderCollection: function () {
+      const thisColl = this;
       /* find collection container */
       const collectionContainer = document.querySelector('.books-list');
-      for (let bookData of thisApp.data.books) {
+      for (let bookData of thisColl.data.books) {
         console.log(bookData);
         /* generate HTML based on temlate */
         const generatedHTML = templates.bookItem(bookData);
@@ -26,17 +56,19 @@
       }
     },
     init: function () {
-      const thisApp = this;
+      const thisColl = this;
       console.log('*** App starting ***');
-      console.log('thisApp:', thisApp);
+      console.log('thisApp:', thisColl);
       console.log('templates:', templates);
-      thisApp.initData();
-      thisApp.initCollection();
+      thisColl.initData();
+      thisColl.renderCollection();
+      thisColl.getElements();
+      thisColl.initActions();
     },
     initData: function () {
-      const thisApp = this;
-      thisApp.data = dataSource;
+      const thisColl = this;
+      thisColl.data = dataSource;
     },
   };
-  app.init();
+  coll.init();
 }
