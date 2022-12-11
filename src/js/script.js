f6c84d85fb9ce8b10e5bc18413ffba9e04f3d24a
Mateusz Kretowski <matkretkurs@gmail.com>
2022-12-12 00:03:25 +0100

    Add hideBooks function

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,7 @@
       bookItem: '#template-book',
     },
     booksList: '.books-list',
+    filters: '.filters',
   };
 
   const templates = {
@@ -15,17 +16,49 @@
     getElements() {
       const thisColl = this;
       thisColl.favoriteBooks = [];
+      thisColl.filters = [];
       thisColl.allBooks = document.querySelector(select.booksList);
-      console.log(thisColl.allBooks);
+      thisColl.filtersAll = document.querySelector(select.filters);
     },
     initActions: function () {
       const thisColl = this;
-      console.log(thisColl);
       /* add addToFavourites as event listener for all books */
       thisColl.allBooks.addEventListener('dblclick', function (event) {
         event.preventDefault();
         thisColl.addToFavorites(event);
       });
+      thisColl.filtersAll.addEventListener('change', function (event) {
+        event.preventDefault();
+        thisColl.addFilter(event);
+        thisColl.hideBooks();
+      });
+    },
+    addFilter: function (event) {
+      const thisColl = this;
+      const thisFilter = event.target;
+      if (!thisFilter.checked) {
+        thisColl.filters.splice(thisColl.filters.indexOf(thisFilter.value, 1));
+      } else {
+        thisColl.filters.push(thisFilter.value);
+      }
+      console.log(thisColl.filters);
+    },
+    hideBooks: function () {
+      const thisColl = this;
+      for (let book of thisColl.data.books) {
+        let bookImage = book.element.querySelector('.book__image');
+        if (bookImage.classList.contains('hidden')) {
+          bookImage.classList.remove('hidden');
+        }
+        console.log(book.details);
+        for (let filter of thisColl.filters) {
+          if (!book.details[filter] && !bookImage.classList.contains('hidden')) {
+            bookImage.classList.add('hidden');
+            console.log(book.details, filter);
+            break;
+          }
+        }
+      }
     },
     addToFavorites: function (event) {
       const thisColl = this;
@@ -38,9 +71,9 @@
           thisBook.classList.add('favorite');
           thisColl.favoriteBooks.push(thisBook.getAttribute('data-id'));
         }
-        console.log(thisColl.favoriteBooks);
       }
     },
+
     renderCollection: function () {
       const thisColl = this;
       /* find collection container */
@@ -58,7 +91,7 @@
     init: function () {
       const thisColl = this;
       console.log('*** App starting ***');
-      console.log('thisApp:', thisColl);
+      console.log('thisColl:', thisColl);
       console.log('templates:', templates);
       thisColl.initData();
       thisColl.renderCollection();
